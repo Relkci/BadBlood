e17c8002a262532f637933b49bb4d2d8c7b758a5
Relkci <29710634+Relkci@users.noreply.github.com.>
2025-03-09 06:11:13 -0600

    laps

--- AD_LAPS_Install/InstallLAPSSchema.ps1
+++ AD_LAPS_Install/InstallLAPSSchema.ps1
@@ -3,12 +3,19 @@ function Get-ScriptDirectory {
 }
 $scriptPath = Get-ScriptDirectory
 
-copy-item -path ($scriptpath + "\admpwd.ps") -destination "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+# copy-item -path ($scriptpath + "\admpwd.ps") -destination "C:\Windows\System32\WindowsPowerShell\v1.0\Modules"
+if (-not (Test-Path "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\admpwd.ps")) { Copy-Item -Path ($scriptpath + "\admpwd.ps") -Destination "C:\Windows\System32\WindowsPowerShell\v1.0\Modules" } else { Write-Host "File 'admpwd.ps' already exists in the destination." }
+
 get-childitem -path ($scriptpath + "\admpwd.ps") -recurse |Foreach-object {
-    Copy-item -literalpath $_.fullname -destination "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\admpwd.ps"
+    #Copy-item -literalpath $_.fullname -destination "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\admpwd.ps"
+    if (-not (Test-Path "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\admpwd.ps")) { Copy-Item -LiteralPath $_.FullName -Destination "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\admpwd.ps" } else { Write-Host "File 'admpwd.ps' already exists in the destination." }
+
 }
-copy-item -path ($scriptpath + "\AdmPwd.admx") -destination "C:\Windows\PolicyDefinitions"
-copy-item -path ($scriptpath + "\AdmPwd.adml") -destination "C:\Windows\PolicyDefinitions\en-US"
+# copy-item -path ($scriptpath + "\AdmPwd.admx") -destination "C:\Windows\PolicyDefinitions"
+if (-not (Test-Path "C:\Windows\PolicyDefinitions\AdmPwd.admx")) { Copy-Item -Path ($scriptpath + "\AdmPwd.admx") -Destination "C:\Windows\PolicyDefinitions" } else { Write-Host "File 'AdmPwd.admx' already exists in the destination." }
+
+#copy-item -path ($scriptpath + "\AdmPwd.adml") -destination "C:\Windows\PolicyDefinitions\en-US"
+if (-not (Test-Path "C:\Windows\PolicyDefinitions\en-US\AdmPwd.adml")) { Copy-Item -Path ($scriptpath + "\AdmPwd.adml") -Destination "C:\Windows\PolicyDefinitions\en-US" } else { Write-Host "File 'AdmPwd.adml' already exists in the destination." }
 
 Import-Module ADMPwd.ps
 Update-AdmPwdADSchema
